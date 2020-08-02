7953c76b0ef0fec8a63ec7454bc48fb5bb1c7608
Gleidson FerSanP <gleidson.ferreirasantos@gmail.com>
2020-08-01 21:07:19 -0300

    add k8s files to deploy

--- api/src/main/java/br/com/dbccompany/api/app/ApiConfig.java
+++ api/src/main/java/br/com/dbccompany/api/app/ApiConfig.java
@@ -2,6 +2,8 @@ package br.com.dbccompany.api.app;
 
 import br.com.dbccompany.core.CoreConfig;
 import br.com.dbccompany.core.config.FeignConfig;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -9,5 +11,6 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @ComponentScan(basePackageClasses = ApiConfig.class)
 @Import({ CoreConfig.class, FeignConfig.class })
+@EnableAutoConfiguration(exclude = KafkaAutoConfiguration.class)
 public class ApiConfig {
 }
